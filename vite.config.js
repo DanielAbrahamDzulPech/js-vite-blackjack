15a40bafeb1e1d301057ba50b892a56dc25e0dc4
Daniel Abraham Dzul Pech <danieldayamond@gmail.com>
2025-01-12 01:50:57 -0500

    added Readme changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig ({
+    // https://danielabrahamdzulpech.github.io/js-vite-blackjack/ motivo
     // debe de llevar las dos / base del directorio  /
     base: '/js-vite-blackjack/',
     plugins: []
