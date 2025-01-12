65b9f769976b3e5fbeefd47ad8a5e27465b3089a
Daniel Abraham Dzul Pech <danieldayamond@gmail.com>
2025-01-12 01:47:17 -0500

    vite.config updated

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig ({
+    // debe de llevar las dos / base del directorio  /
+    base: '/js-vite-blackjack/',
+    plugins: []
+});
